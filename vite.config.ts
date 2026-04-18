94e1884995c7796e1004ddccc9e6ff0bbe66c0c2
Théo P. Niggemann <i0560008@gmail.com>
2026-04-18 13:45:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// defineConfig é uma função do Vite que recebe as configurações do projeto.
-// Ela dá autocomplete e tipagem no editor — é uma boa prática sempre usar.
+
 export default defineConfig({
-  plugins: [react()], // Ativa o plugin React: transforma JSX/TSX e habilita Fast Refresh
+  plugins: [react()], 
 })
